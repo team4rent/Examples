03011aba53edbef0648357d66cbc2a89c5c86138
Sasha Vlaisavljevich <info@team4rent.com>
2018-05-11 09:15:41 +0200

    enter

--- ReactJS/projectmanager/src/App.js
+++ ReactJS/projectmanager/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Projects from './Components/Projects'
 import './App.css';
 
 class App extends Component {
+
+  constructor(){
+    super();
+    
+    this.state = {
+      projects: [
+        {
+          title: "Busines website",
+          category: "Web Design"
+        },
+        {
+          title: "Social App",
+          category: "Mobile Development"
+        },
+        {
+          title: "Ecommerce Shopping Cart",
+          category: "Web Development"
+        }
+      ]
+    }
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        My App
+        <Projects projects={this.state.projects} />
       </div>
     );
   }
